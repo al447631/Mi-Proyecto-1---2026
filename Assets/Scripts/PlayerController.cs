c2a75c308366698f6b82ad2f66c211cef97455bf
Torrent9933 <al447631@uji.es>
2026-05-07 09:50:17 +0200

    Mas Avances
    Arreglo Movimiento - Suelo
    Animaciones Introducias ( Jump Y Run)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,44 +3,63 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    
     [SerializeField] private float velocidad = 5f;
     [SerializeField] private float potenciaSalto = 7f;
+    [SerializeField] private LayerMask capaSuelo; // Detectar suelo
+    [SerializeField] private Transform controladorSuelo; 
+    [SerializeField] private Vector2 dimensionesCaja = new Vector2(0.8f, 0.2f);
+    
     private Vector2 direccionMovimiento;
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
+    public Animator anim;
+    [SerializeField] private bool enSuelo;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
-    public void OnMove (InputValue  value)
+    public void OnMove (InputValue value)
     {
         direccionMovimiento = value.Get<Vector2>();
     }
+    
     public void OnJump (InputValue value)
     {
-        if (value.isPressed)
+        if (value.isPressed && enSuelo)
         {
-            rb.linearVelocity = new Vector2(rb.linearVelocityX, potenciaSalto);
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, potenciaSalto);
         }
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void FixedUpdate()
     {
-        //Para que se mueva el jugador
-        rb.linearVelocity = new Vector2(direccionMovimiento.x*velocidad, rb.linearVelocity.y);
+        // 1. Detectamos el suelo correctamente
+        Collider2D objetoTocado = Physics2D.OverlapBox(controladorSuelo.position, dimensionesCaja, 0f, capaSuelo);
 
-        //Girar Sprite
+        if (objetoTocado != null)
+        {
+            enSuelo = true;
+        }
+        else
+        {
+            enSuelo = false;
+        }
+
+        
+
+        // 2. Para que se mueva el jugador
+        rb.linearVelocity = new Vector2(direccionMovimiento.x * velocidad, rb.linearVelocity.y);
+
+        // 3. Girar Sprite
         if(direccionMovimiento.x > 0 && sprite.flipX)
         {
             sprite.flipX = false; //Derecha
@@ -52,5 +71,8 @@ public class PlayerController : MonoBehaviour
                 sprite.flipX = true; //Izquierda
             }
         }
+
+        anim.SetFloat("VelocidadX", Mathf.Abs(rb.linearVelocity.x));
+        anim.SetBool("EnSuelo", enSuelo);
     }
 }
