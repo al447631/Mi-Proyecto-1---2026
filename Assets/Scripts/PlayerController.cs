ae0dffb9941d2f6e0d396fb06d70b431d0e1c063
Torrent9933 <al447631@uji.es>
2026-05-08 05:24:01 +0200

    Cambios y Afinamiento de Animaciones Pj
    Quitamos Agachar daba problemas
    Y terminamos Shoot (incluido con salto)
    Tambien Añadimos proyectil y su animacion.
    De momento PJ terminado. :)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,12 +8,15 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private LayerMask capaSuelo; // Detectar suelo
     [SerializeField] private Transform controladorSuelo; 
     [SerializeField] private Vector2 dimensionesCaja = new Vector2(0.8f, 0.2f);
+    [SerializeField] private GameObject ArmaPrefab;
+    [SerializeField] private Transform firePoint;
     
     private Vector2 direccionMovimiento;
     private Rigidbody2D rb;
     private SpriteRenderer sprite;
     public Animator anim;
     [SerializeField] private bool enSuelo;
+    
 
     void Awake()
     {
@@ -34,10 +37,29 @@ public class PlayerController : MonoBehaviour
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, potenciaSalto);
         }
     }
+    
+    
+    public void OnShoot(InputValue value) 
+    {
+    if (value.isPressed)
+        {
+            anim.SetTrigger("Shoot");
+
+            // Direccion Visual del PJ
+            float direccionDisparo = sprite.flipX ? -180f : 0f;
+            
+            // Rotacion punto disparo
+            firePoint.rotation = Quaternion.Euler(0, 0, direccionDisparo);
+
+            // Creador proyectil
+            Instantiate(ArmaPrefab, firePoint.position, firePoint.rotation);
+        }
+    
+    }
 
     void Start()
     {
-        
+    
     }
 
     void FixedUpdate()
@@ -54,7 +76,7 @@ public class PlayerController : MonoBehaviour
             enSuelo = false;
         }
 
-        
+       
 
         // 2. Para que se mueva el jugador
         rb.linearVelocity = new Vector2(direccionMovimiento.x * velocidad, rb.linearVelocity.y);
@@ -74,5 +96,7 @@ public class PlayerController : MonoBehaviour
 
         anim.SetFloat("VelocidadX", Mathf.Abs(rb.linearVelocity.x));
         anim.SetBool("EnSuelo", enSuelo);
+
+        
     }
 }
